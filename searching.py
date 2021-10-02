b656ab1a995ea675d0f42871759eb5a332ff8021
Andryxa <andron_03333@mail.ru>
2021-10-02 19:07:48 +0300

    Type in console you request to get answer. Changed formattedUrl to link. Changed htmlTitle to title. Added link for image. If there is no image, returns 'NULL'.

--- searching.py
+++ searching.py
@@ -1,14 +1,16 @@
 import googleapiclient.discovery as gc
 import pprint
-
+question = input()
 def main():
     service = gc.build("customsearch", "v1", developerKey= "AIzaSyC3EeNpQWJKlNrsLWY2BeERN7lsCK55VyE")
-    res = service.cse().list(q = 'Турбулентность',cx = '8fa0dc9f3a0a72baa').execute()
+    res = service.cse().list(q = question,cx = '8fa0dc9f3a0a72baa').execute()
     links = []
-    #pprint.pprint(res) Для красивого отображения запроса
+    pprint.pprint(res) #Для красивого отображения запроса
     print()
-    for i in range(len(res['items'])):
-        links.append(res['items'][i]['htmlTitle'], res['items'][i]['formattedUrl'])
+    for iter in range(10):
+        links.append([res['items'][iter]['title'], res['items'][iter]['link'],
+                      res['items'][iter]['pagemap']['cse_image'][0]['src']
+                      if 'cse_image' in res['items'][iter]['pagemap'].keys() else 'NULL'])
     print(links)
 
 if __name__ == '__main__':
